01d0bc14b3cbb0f08bca4026e7bdff6b28a69951
jd003walker <41433222+jd003walker@users.noreply.github.com>
2019-02-08 21:53:41 +0530

    Fixed dynamic dashboard

--- lib/home/dashboard.dart
+++ lib/home/dashboard.dart
@@ -15,52 +15,56 @@ class _DashBoardState extends State<DashBoard> {
   Widget build(BuildContext context) {
     return Container(
 //      color: Colors.red,
-      width: 1000,
+      width: 1500,
       margin: EdgeInsets.all(10.0),
 //                    child: Image.asset(
 //                      "images/profile_overview.png",
 //                      fit: BoxFit.fill,
 //                    ),
-      child: Card(
-        color: customColor.primaryColor,
-        child: Container(
-          width: 1000,
-          height: 180,
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(top:24.0,right:24.0,left: 8.0),
-                child: CircleAvatar(
-                  radius: 55.0,
-                  backgroundImage: widget.user!=null?NetworkImage(widget.user.photoUrl):Image.asset("face.png"),
-                ),
-              ),
-              Column(
+      child: Container(
+              child: Card(
+          color: customColor.primaryColor,
+                    child: Container(
+              width: 1500,
+              height: 180,
+              child: Row(
                 crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                // direction: Axis.horizontal,
                 children: <Widget>[
-                  SizedBox(height: 30.0,),
-                  Text(
-                    "Hello, ${widget.user.displayName[0].toUpperCase()+
-                        widget.user.displayName.substring(1, widget.user.displayName.indexOf(" "))+"!"}",
-                    style: TextStyle(color: Colors.white, fontSize: 30.0),
-                  ),
-                  SizedBox(height: 15.0,),
-                  Text(widget.user.email,style: TextStyle(color: Colors.white,fontSize: 16.0),),
-
-                  InkWell(
-                    onTap: ()=>print("More tapped!!!"),
-                    child: Padding(
-                      padding: const EdgeInsets.only(top:50.0,left: 120.0,right:20.0),
-                      child: Text("More",style: TextStyle(color: Colors.white,fontSize: 24.0),),
+                  Padding(
+                    padding: const EdgeInsets.only(top:24.0,right:18.0,left: 8.0),
+                    child: CircleAvatar(
+                      radius: 55.0,
+                      backgroundImage: widget.user!=null?NetworkImage(widget.user.photoUrl):Image.asset("face.png"),
                     ),
                   ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      SizedBox(height: 30.0,),
+                      Text(
+                        "Hello, ${widget.user.displayName[0].toUpperCase()+
+                            widget.user.displayName.substring(1, widget.user.displayName.indexOf(" "))+"!"}",
+                        style: TextStyle(color: Colors.white, fontSize: 30.0),
+                      ),
+                      // SizedBox(height: 15.0,),
+                      // Text(widget.user.email,style: TextStyle(color: Colors.white,fontSize: 16.0),),
 
+                      InkWell(
+                        onTap: ()=>print("More tapped!!!"),
+                        child: Padding(
+                          padding: const EdgeInsets.only(top:50.0,left: 120.0,right:20.0),
+                          child: Text("More",style: TextStyle(color: Colors.white,fontSize: 24.0),),
+                        ),
+                      ),
+
+                    ],
+                  ),
                 ],
               ),
-            ],
-          ),
+            ),
+
         ),
       ),
     );
